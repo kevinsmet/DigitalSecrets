bc3c209f2ee3d5d75b717a6e54b67d8d02d1a8fb
Kevin Smet <kevin.smet@cegeka.com>
2017-02-17 09:43:23 +0100

    added spring

---
+++ DigitalSecretBox/src/main/java/com/cegeka/DigitalSecretBox/DigitalSecretBoxApplication.java
@@ -0,0 +1,12 @@
+package com.cegeka.DigitalSecretBox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalSecretBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalSecretBoxApplication.class, args);
+	}
+}
